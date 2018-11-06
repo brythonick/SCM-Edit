fead3e95eba6f1c1bf343632ce50b9667ad29e2f
Nick Farnham <brythonick@gmail.com>
2018-11-06 22:52:26 +0000

    Add ability to change layer top boundary
    Individual layers can now be selected by the user. The upper boundary
    layer can be switched in the lower control panel. Other property
    selectors will be added here.

--- svgeo.js
+++ svgeo.js
@@ -1,10 +1,12 @@
 const margin = 25;
 const canvas = d3.select("svg");
 let canvasSize = $("svg")[0].getBoundingClientRect();
+
 window.addEventListener("resize", () => {canvasSize = $("svg")[0].getBoundingClientRect(); layerStack.redrawAll()}, false);
+document.getElementById("canvas").addEventListener("mouseup", (e) => layerStack.select(e.target.id), false);
 
 
-class Boundary {
+class BoundaryData {
     static flat(x, y) {
         return [
             {"x": x, "y": y},
@@ -16,7 +18,6 @@ class Boundary {
         let lineData = [];
         for (let i = 0; i <= 10; i += 1) {
             let zigzagY = (i % 2 === 0 ? y + 10 : y - 10);
-            console.log(i + ": " + zigzagY);
             lineData.push({"x": i/10.0 * canvasSize.width, "y": zigzagY});
         }
         return lineData;
@@ -27,6 +28,23 @@ class Boundary {
             .x((d) => {return d.x;})
             .y((d) => {return d.y;});
         return line(data);
+    }
+}
+
+
+class Boundary {
+    constructor(type) {
+        this.type = type;
+        this.data = this.data.bind(this);
+    }
+
+    data(x, y) {
+        switch (this.type) {
+            case "Flat":
+                return BoundaryData.flat(x, y);
+            case "Zigzag":
+                return BoundaryData.zigzag(x, y);
+        }
     }
 }
 
@@ -52,19 +70,27 @@ class Layer {
         this.lowerBoundary = lowerBoundary;
         this.x = x;
         this.y = y;
-        let combined = upperBoundary(x, y).concat(lowerBoundary(x, y + 50).reverse());
+        let combined = upperBoundary.data(x, y).concat(lowerBoundary.data(x, y + 50).reverse());
         canvas.append("path")
-            .attr("d", Boundary.toLine(combined) + "Z")
+            .attr("d", BoundaryData.toLine(combined) + "Z")
             .attr("class", "layer " + this.type)
-            .attr("id", "layer" + this.id);
+            .attr("id", this.id)
+            .attr("pointer-events", "visibleFill");
+        // document.getElementById(this.id).addEventListener("click", () => layerStack.select(this.id), false);
     }
 
     erase() {
-        d3.select("#layer" + this.id).remove();
+        d3.select("#" + this.id).remove();
+    }
+
+    setUpperBoundary(type) {
+        this.upperBoundary = new Boundary(type);
+        layerStack.redrawAll();
     }
 
     redraw() {
         this.erase();
+        this.lowerBoundary = layerStack.getBoundaryBelow(this.id);
         this.draw(this.type, this.x, this.y, this.upperBoundary, this.lowerBoundary);
     }
 }
@@ -73,8 +99,10 @@ class Layer {
 class LayerStack {
     constructor() {
         this.layers = [];
+        this.selected = null;
         this.new = this.new.bind(this);
         this.pop = this.pop.bind(this);
+        this.select = this.select.bind(this);
     }
 
     get yCoordinate() {
@@ -83,19 +111,29 @@ class LayerStack {
 
     get currentTopBoundary() {
         if (this.layers.length === 0) {
-            return Boundary.zigzag;
+            return new Boundary("Flat");
         } else {
             return this.layers[this.layers.length - 1].boundary;
         }
     }
 
+    getBoundaryBelow(id) {
+        const upperLayer = this.layers.filter(layer => layer.id === id)[0];
+        const index = this.layers.indexOf(upperLayer);
+        if (index === 0) {
+            return upperLayer.lowerBoundary;
+        } else {
+            return this.layers[index - 1].boundary;
+        }
+    }
+
     new(type) {
-        const newLayer = new Layer(this.layers.length);
+        const newLayer = new Layer("layer" + this.layers.length);
         newLayer.draw(
             type,
             0,
             this.yCoordinate,
-            Boundary.flat,
+            new Boundary("Flat"),
             this.currentTopBoundary
         );
         this.layers.push(newLayer);
@@ -108,6 +146,11 @@ class LayerStack {
         }
     }
 
+    select(id) {
+        this.selected = this.layers.filter(layer => layer.id === id)[0];
+        document.getElementById("topBoundary").value = this.selected.boundary.type;
+    }
+
     redrawAll() {
         this.layers.forEach((layer) => {layer.redraw()})
     }
@@ -116,3 +159,5 @@ let layerStack = new LayerStack();
 document.getElementById("new-limestone").addEventListener("click", () => layerStack.new("limestone"), false);
 document.getElementById("new-shale").addEventListener("click", () => layerStack.new("shale"), false);
 document.getElementById("pop-layer").addEventListener("click", layerStack.pop, false);
+
+document.getElementById("topBoundary").addEventListener("change", () => layerStack.selected.setUpperBoundary(document.getElementById("topBoundary").value), false);
